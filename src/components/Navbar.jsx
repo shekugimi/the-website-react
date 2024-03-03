4cb9cf5a06baa58299e796a994da0cc884042923
Satyam Singh <satyam.12011172@gmail.com>
2024-03-03 06:29:03 +0530

    some checks updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,33 +20,29 @@ const Navbar = () => {
   }, []);
 
   const handleMenuClick = () => {
-    setMenuOpen(true);
+    setMenuOpen(!menuOpen);
   };
 
-  const handleCloseClick = () => {
-    setMenuOpen(false);
-  };
 
   return (
 
-
     <nav class="border-gray-200 dark:bg-gray-900 p-2">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto ">
-        <Link to="/" className=" flex items-center text-start gap-0 line-h-0 p-1 ">
-          <img src={logo} alt="Logo" className=" w-[60px] flex text-white transition-all duration-300"
-          />
+        <Link to="/" className="flex items-center text-start gap-0 line-h-0 p-1 ">
+          <img src={logo} alt="Logo" className=" w-[60px] flex text-white transition-all duration-300"/>
           <span className="text-lg font-bold">Progyan <br /> Educations</span>
         </Link>
-        <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-          <span class="sr-only">Open main menu</span>
+        <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false" 
+        onClick={()=>handleMenuClick()}>
+          {/* <span class="sr-only">Open main menu</span> */}
           <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
           </svg>
         </button>
         <div class="hidden w-full md:block md:w-auto " id="navbar-default">
           <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border md:p-2 md:px-4 border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <a href="/home" class="block py-2 px-3 text-white  rounded hover:bg-transparent  md:p-0 dark:text-white md:hover:text-blue-700  " aria-current="page">Home</a>
+          <li>
+              <a href="/" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Home</a>
             </li>
             <li>
               <a href="/about" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
@@ -55,10 +51,10 @@ const Navbar = () => {
               <a href="/courses" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Courses</a>
             </li>
             <li>
-              <a href="/contact" class="block  py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ">Contact</a>
+              <a href="/contact" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ">Contact</a>
             </li>
             <li>
-              <a href="/register" class="border border-2 border-white hover:bg-white hover:text-black font-bold p-2 rounded rounded-lg ">Join Now</a>
+              <a href="/register" class="border border-2 text-gray-900 border-white hover:bg-white hover:text-black font-bold p-2 rounded rounded-lg">Join Now</a>
             </li>
           </ul>
         </div>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -23,30 +23,6 @@ const Header = () => {
             of Artificial Intelligence, we offer innovative learning solutions
             designed to prepare students for the demands of the modern world.
           </p>
-          <a
-            href="/courses"
-            className="button_primary border border-2 border-white rounded rounded-2xl inline-flex items-center hover:bg-gray-700 justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
-          >
-            Get started
-            <svg
-              className="w-5 h-5 ml-2 -mr-1"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </a>
-          <a
-            href="#"
-            className="inline-flex bg-gray-300 text-black hover:bg-gray-400 border border-2 border-white rounded rounded-2xl text-start items-center justify-center px-5 py-3 font-medium hover:text-gray-950 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100  "
-          >
-            Speak to Sales
-          </a>
         </div>
         <div className="hidden lg:mt-0 lg:col-span-5 md:flex">
           <img src={headerImage} alt="mockup" />
